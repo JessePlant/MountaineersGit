f318c250f6f00d30399e2a8a9cc4c3948802d724
Jesse Plant <jesseplan2002@gmail.com>
2024-10-12 15:35:11 +0200

    Title Screen Play button working

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
-   public void goToFinalMovementScene()
-   {
-    SceneManager.LoadScene("FinalMovement");
-    Debug.Log("Play button pressed, loading new scene");
-   }
+   
+    public void goToFinalMovementScene()
+    {
+        SceneManager.LoadScene("FinalMovement");
+        Debug.Log("Play button pressed, loading new scene");
+    }
+
+    public void Quit()
+    {
+        //Here will close game.
+    }
 }
