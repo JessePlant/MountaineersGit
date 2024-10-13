9966d3f3b6e58f0c914981409da99d11c46de806
Jono <jono1711@outlook.com>
2024-10-13 16:15:07 +0200

    Added the particle System and Changed the Enemy Behaviour to include the slider on his headtop

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,70 +3,127 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Scripting;
+using UnityEngine.UI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
     // Start is called before the first frame update
     public NavMeshAgent agent;
     public Transform Gert;
+
+    public Transform Emily;
     public float moveSpeed;
     public float timeBetweenAttacks = 5f;
-    public HealthManager healthManager;
     public bool alreadyAttacked;
     public float attackRange;
     public bool inAttackRange;
     public LayerMask playerLayer;
-    public float enemyHealth = 30f;
+    public AttackController attackController;
+    public CameraController cameraController;
+    PhysicalState GertState;
+    PhysicalState EmilyState;
+    public Transform target;
+    int RandomTarget;
 
-    void Start(){
+    [Header("Health Stuff")]
+    public Slider healthBar;
+    public Vector3 healthBarOffset= new Vector3(0,1,0);
+    public float maxHealth = 30f;
+    public float currentHealth = 30f;
+    public bool canMove = true;
+    void Awake(){
         Gert = GameObject.Find("Gert").GetComponent<Transform>();
+        Emily = GameObject.Find("Emily").GetComponent<Transform>();
         agent = GetComponent<NavMeshAgent>();
+        GertState = GameObject.Find("Gert").GetComponent<PhysicalState>();
+        EmilyState = GameObject.Find("Emily").GetComponent<PhysicalState>();
+        attackController = GameObject.Find("AttackController").GetComponent<AttackController>();
         agent.autoTraverseOffMeshLink = true;
         attackRange = 1f;
         alreadyAttacked = false;
+        RandomTarget = Random.Range(0, 1);
+        if(RandomTarget == 0){
+            target = Gert;
+            Debug.Log("Current Target is Gert");
+        }
+        else{
+            target = Emily;
+             Debug.Log("Current Target is Emily");
+        }
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
     }
 
    
     // Update is called once per frame
     void Update()
     {
-        
+        if(!SherpaShopKeeper.isInShop && canMove)
+        {
+        healthBar.transform.position = transform.position + healthBarOffset;
+        healthBar.transform.LookAt(Camera.main.transform); 
         inAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
         if(!inAttackRange) Chase();
         else Attack();
+        }
     }
-    public void Chase(){
-        agent.SetDestination(Gert.position);
+    public void Chase()
+    {
+        agent.SetDestination(target.position);
     }
 
-    public void Attack(){
+    public void Attack()
+    {
         Debug.Log("Going to Attack");
         agent.SetDestination(transform.position);
-        transform.LookAt(Gert);
         Debug.Log("Already Attacked "+alreadyAttacked);       
         if(!alreadyAttacked){
             Debug.Log("Attack Commencing");
             alreadyAttacked = true;
-            healthManager.Playerdmg();
+            // healthManager.Playerdmg();
+            if(RandomTarget == 0 )
+            {
+                GertState.Damage(10);
+            }
+            else
+            {
+                EmilyState.Damage(10);
+            }
             StartCoroutine(ResetAttack());
         }
     }
-    IEnumerator ResetAttack(){
-    Debug.Log("Coroutine started, waiting for: " + timeBetweenAttacks + " seconds");
-        yield return new WaitForSecondsRealtime(timeBetweenAttacks);
+
+    IEnumerator ResetAttack()
+    {
+        Debug.Log("Coroutine started, waiting for: " + timeBetweenAttacks + " seconds");
+        yield return new WaitForSecondsRealtime(3f);
         Debug.Log("Starting Coroutine");
         alreadyAttacked = false;
     }
     
     public void TakeDamage(float damage){
-        enemyHealth -= damage;
-        print(enemyHealth);
-        if(enemyHealth <= 0){
+        currentHealth -= damage;
+        print("Current Health"+currentHealth);
+        print(currentHealth);
+        if(currentHealth <= 0){
             Destroy(gameObject);
             SaveManager.Instance.money += 5;
+            SaveManager.Instance.Save();
         }
         else{
-            agent.SetDestination(transform.position);
+            healthBar.value = currentHealth;
+            StartCoroutine(shellShock());
         }
+    }
+    IEnumerator shellShock()
+    {
+        canMove = false;
+        if(attackController.currentGun.name == "Lazer Rifle"){
+            yield return new WaitForSecondsRealtime(1);
+        }
+        else{
+            yield return new WaitForSecondsRealtime(0.5f);
+        }
+        canMove = true;
     }
 }
