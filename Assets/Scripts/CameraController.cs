3535ae80917be9093861d85a4fb61afcd0f0ee28
Jesse Plant <jesseplan2002@gmail.com>
2024-10-14 03:27:24 +0200

    Added character Sprites

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     public bool onGert; //If true player is controlling Gert, else controlling Emily.
     public float screenTopY;
+    public GameObject EmilySprite, GertSprite;
     public float scrollThreshold = 1.0f;
     public float scrollSpeed = 5.0f;
      public float smoothSpeed = 0.125f;
@@ -23,8 +24,10 @@ public class CameraController : MonoBehaviour
         target2 = GameObject.Find("Emily");
         //Set default angle to Gert.
         Camera.main.orthographicSize = cameraSize;
-        
+        EmilySprite = GameObject.Find("EmilySprite");
+        GertSprite = GameObject.Find("GertSprite");
         //Here some logicc to find centre of mountain. 
+        
         mountMid = new Vector3 (0, 0.5f, 0);
         Camera.main.transform.position = target1.transform.position + new Vector3(0,0,-17); //NEEDTO redo this with the current wall surface normal force direction times5. ie opposite direction player facing.
         //screenTopY = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y;
@@ -40,6 +43,8 @@ public class CameraController : MonoBehaviour
             
             //Also want to make the camera slowly move over to selected char, so the tab function should also be on a timer
         }
+        EmilySprite.transform.LookAt(target2.transform.position);
+        GertSprite.transform.LookAt(target1.transform.position);
         if(onGert)
         {
             targ1Pos = target1.transform.position;
@@ -47,7 +52,8 @@ public class CameraController : MonoBehaviour
             Vector3 Dir = mountMid-targ1Pos;
             if (Physics.Raycast(target1.transform.position, Dir, out Hit ))
             {
-                Camera.main.transform.position = targ1Pos + Hit.normal*17; //Hit.normal is direction we need camera to move from player.
+                GertSprite.transform.position = targ1Pos + Hit.normal.normalized*0.1f;
+                Camera.main.transform.position = targ1Pos + Hit.normal.normalized*13; //Hit.normal is direction we need camera to move from player.
             }
             Camera.main.transform.LookAt(targ1Pos);
             //Camera.main.transform.position = target1.transform.position + new Vector3(0,0,-17); //NEEDTO redo this as above.
@@ -59,7 +65,8 @@ public class CameraController : MonoBehaviour
             Vector3 Dir = mountMid-targ2Pos;
             if (Physics.Raycast(target2.transform.position, Dir, out Hit ))
             {
-                 Camera.main.transform.position = targ2Pos + Hit.normal*17;
+                EmilySprite.transform.position = targ2Pos + Hit.normal.normalized*0.1f;
+                Camera.main.transform.position = targ2Pos + Hit.normal*13;
             }
             Camera.main.transform.LookAt(targ2Pos);
             //Camera.main.transform.position = target2.transform.position + new Vector3(0,0,-17);
